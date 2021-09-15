548fdf4f122c350a3d562040bdee5e3757035d51
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-09-15 15:12:15 +0300

    Add files via upload

---
+++ Server/services/itemService.js
@@ -0,0 +1,33 @@
+const Item = require('../models/Item')
+
+async function getAll() {
+    return await Item.find({}).lean()
+}
+
+async function getById(id) {
+    return await Item.findById(id)
+}
+
+async function create(data) {
+    const result = new Item(data);
+    await result.save()
+    return result;
+}
+
+async function update(original, updated) {
+    Object.assign(original, updated)
+    await original.save()
+    return original;
+}
+
+async function remove(id){
+    return Item.findByIdAndDelete(id)
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create, 
+    update,
+    remove
+}
