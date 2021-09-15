e2699b65790ecc27e214c1bede61fbbc7c644c40
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-09-15 16:05:48 +0300

    Update Item.js

--- Server/models/Item.js
+++ Server/models/Item.js
@@ -5,6 +5,7 @@ const schema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     price: {type: Number, required: true},
+    imageUrl: {type: String, required: true, match:[/^https?/, 'Image must be valid URL']},
     owner: {type: Schema.Types.ObjectId, ref: 'User'}
 })
 
