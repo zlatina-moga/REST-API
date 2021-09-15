a093672451fef6d3a75b60bc446356689e80ca5b
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-09-15 14:36:06 +0300

    Add files via upload

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const dbConfig = require('./config/database')
+const {PORT} = require('./config/index')
+const cors = require('./middlewares/cors')
+const userController = require('./controllers/userController')  // adjust to project requirements
+const itemController = require('./controllers/itemController')  // adjust to project requirements
+
+start()
+
+async function start(){
+    const app = express()
+    await dbConfig(app)
+    app.use(cors())
+    app.use(express.json())
+
+    app.use('/users', userController) // adjust to project requirements
+    app.use('/data/catalog', itemController) // adjust to project requirements
+
+    app.listen(PORT, () => console.log(`REST Service is running on port ${PORT}`))
+}
