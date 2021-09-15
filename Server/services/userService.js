4989b1e4003ab7bc527c932652cf75775914f679
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-09-15 14:29:56 +0300

    Add files via upload

---
+++ Server/services/userService.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User');
+const {SECRET} = require('../config')
+
+async function register(email, password) {
+    const existing = await User.findOne({email});
+    if (existing){
+        const err = new Error('Email is already in use');
+        //err.status(409);
+        throw err;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = new User({
+        email,
+        hashedPassword
+    })
+
+    await user.save()
+
+    return {
+        _id: user._id,
+        email: user.email,
+        accessToken: generateToken(user)
+    };
+}
+
+async function login(email, password){
+    const user = await User.findOne({email})
+
+    if (!user) {
+        const err = new Error('Incorrect email or password');
+        err.status = 401;
+        throw err;
+    }
+
+    const match = await bcrypt.compare(password, user.hashedPassword)
+
+    if (!match) {
+        const err = new Error('Incorrect email or password');
+        err.status = 401;
+        throw err;
+    }
+
+    return {
+        _id: user._id,
+        email: user.email,
+        accessToken: generateToken(user)
+    }
+}
+
+
+function generateToken(user){
+    const token = jwt.sign({
+        _id: user._id,
+        email: user.email
+    }, SECRET)
+
+    return token;
+}
+
+module.exports = {
+    register,
+    login
+}
